75fba6be876c9ecc38724590c0f37bb7673910b9
grez72 <grez72@gmail.com>
2020-02-19 12:43:06 -0500

    added image download script

---
+++ images/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/7tqlvb69lvx570h/BaldBear.jpg -q --show-progress -O /home/jovyan/images/BaldBear.jpg
+wget https://www.dropbox.com/s/lv6tjxuc90mksgc/BrownBear.jpg -q --show-progress -O /home/jovyan/images/BrownBear.jpg
+wget https://www.dropbox.com/s/65p68g331kby809/Gorilla.jpg -q --show-progress -O /home/jovyan/images/Gorilla.jpg
